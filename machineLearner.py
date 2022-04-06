b44221d28e8feb92c23ee0f67513e3e29f6d2c82
HAL703 <eonsago703@protonmail.ch>
2022-04-05 21:46:56 -0400

    Added ability to loop program

--- machineLearner.py
+++ machineLearner.py
@@ -1,15 +1,14 @@
 from algorithmML import Algorithms as algo
 if __name__ == "__main__":
-    truthValue = True
+    truthValue=True
     print("Please enter the name of an image (.png, .jpg) with its extension.")
     imgStr = str(input())
     algorithms = "k_means,color_detection,bit_masking"
     algorithms = algorithms.split(",")
-    print("Please enter which algorithm to utilize, please enter the number beside the algorithm in the list.\n")
     for count, algorithm in enumerate(algorithms):
         print(count, algorithm)
-    algoStr = int(input())
     while truthValue: 
+        algoStr = int(input("\nPlease enter which algorithm to utilize, enter the number beside the algorithm in the list above: "))
         try:
             if(algoStr == 0):
                 algo.Kmeans(imgStr)
@@ -19,9 +18,14 @@ if __name__ == "__main__":
                 algo.Bmask(imgStr)
             else:
                 raise Exception("Invalid integer entered, please only enter 0, 1, or 2.")
-            truthValue=False
         except Exception:
             print("Invalid value entered, enter another value: ")
             algoStr = int(input())
+        contStr = input("Continue typing algorithms? (Y/N): ")
+        if contStr.upper() == "Y":
+            continue
+        print("Bye...")
+        break
+
 
 
